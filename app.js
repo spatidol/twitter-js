d820c15118b37e8eb703f348cde814e274088540
Allie Spatidol <spatidol@gmail.com>
2016-09-19 17:39:28 -0400

    two

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ var express = require( 'express' );
 var app = express(); // creates an instance of an express application
 
 
+var volleyball = require('volleyball');
+
+app.use(volleyball);
+
+
 app.listen(3000, function() {
   console.log("server listening")
 });
 
 
+
+// app.use(function(request, response, next) {
+//   console.log(request.method, request.originalUrl);
+//   next();
+// })
+
+// app.use("/news", function(request, response, next) {
+//   console.log("You've reached special area");
+//   next();
+// })
+
 app.get("/", function(request, response, next) {
   response.send("Hello Welcome");
+});
+
+app.get("/news", function(request, response, next) {
+  response.send("Hello from news");
 })
