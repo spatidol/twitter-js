11eb66882d3ccadc393392fbcd0bbbe9766b345b
Allie Spatidol <spatidol@gmail.com>
2016-09-20 11:08:26 -0400

    tweets loading

--- app.js
+++ app.js
@@ -7,50 +7,22 @@ var volleyball = require('volleyball');
 
 app.use(volleyball);
 
+var routes = require('./routes/');
+app.use('/', routes);
+
 
 app.listen(3000, function() {
   console.log("server listening")
 });
 
 
-// app.use(function(req, res, next) {
-//   console.log(req.method, req.originalUrl);
-//   next();
-// }
 
-// app.use("/news", function(req, res, next) {
-//   console.log("You've reached special area");
-//   next();
-// })
-
-app.get("/", function(req, res, next) {
-  res.send("Hello Welcome");
-});
-
-app.get("/news", function(req, res, next) {
-  res.send("Hello from news");
-})
-
-
-var locals = {
-    title: 'An Example',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-};
-nunjucks.configure('views', {noCache: true});
-nunjucks.render('index.html', locals,  function (err, output) {
-  if (err) throw Error();
-    console.log(output);
-});
+// app.use("/", function(req, res, next) {
+//   res.render( 'index', {title: 'Hall of Fame', people: people} );
+// });
 
 
 nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 
-
-var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-res.render( 'index', {title: 'Hall of Fame', people: people} );
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+// could use one line instead: var router = require('express').Router();
+var tweetBank = require('../tweetBank');
+
+router.get('/', function (req, res) {
+  var tweets = tweetBank.list();
+  res.render( 'index', { tweets: tweets } );
+});
+
+
+router.use(express.static('public'));
+
+
+module.exports = router;
